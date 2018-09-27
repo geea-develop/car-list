eeaefd22d28aead9961447334656b90f00015854
Guy Ambar <guy@meetleo.co>
2018-09-27 14:06:28 +0300

    added english files

--- index.js
+++ index.js
@@ -2,32 +2,43 @@
 var csv = require("fast-csv");
 var fs = require('fs');
 
-var carManufacturers = {};
+var carManufacturers = require('./carManufactureresEng');
+carManufacturers = Object.keys(carManufacturers);
 var carModels = {};
+var carManufacturersHeb = {};
+
+// const manufacturersEng = require('./carManufactureresEng')
+const carManufacturersEng = require('./out/car-manufacturers-eng');
+// Object.keys(manufacturersEng).forEach(carManufacturer => {
+//     manufacturersEng[carManufacturer].forEach(key => {
+//         carManufacturersEng[key] = carManufacturer;
+//     })
+// });
+// writeToJSON('car-manufacturers-eng', carManufacturersEng, function(){ console.log('carManufacturersEng.json created');});
 
 csv
  .fromPath("DGAMIM-PRATI.csv", {headers : ["type", "manufacturerNo","manufacturerName","modelNo", "modelName", "modelSeries", "engineCapacity", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ", "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BK", "BL", "BM", "BN", "BO" ]})
  .on("data", function(data){
    var car = {
      manufacturerNo: data.manufacturerNo,
-     manufacturerName: data.manufacturerName,
+     manufacturerName: carManufacturersEng[data.manufacturerNo],
      modelNo: data.modelNo,
-     modelName: data.modelName,
-     modelSeries: data.modelSeries,
+     modelName: data.modelName.trim(),
+     modelSeries: data.modelSeries.trim(),
      engineCapacity: data.engineCapacity
    }
-   carManufacturers[data.manufacturerNo] = data.manufacturerName;
+     carManufacturersHeb[data.manufacturerNo] = data.manufacturerName.trim();
 
-   if (!carModels[data.manufacturerNo])
-    carModels[data.manufacturerNo] = [];
+   if (!carModels[car.manufacturerName])
+    carModels[car.manufacturerName] = [];
 
-   carModels[data.manufacturerNo].push(car)
+   carModels[car.manufacturerName].push(car)
 
  })
  .on("end", function(){
     console.log("done reading csv");
 
-    writeToJSON('car-manufacturers', carManufacturers, function(){ console.log('carManufacturers.json created');});
+     writeToJSON('car-manufacturers', carManufacturersHeb, function(){ console.log(`carManufacturersHeb car-manufacturers created`);})
 
     Object.keys(carModels).forEach(key => {
       writeToJSON('models/'+key, carModels[key], function(){ console.log(`carModels for manufacturer ${key} created`);})
@@ -36,5 +47,6 @@ csv
  });
 
 function writeToJSON(fileName, obj, callback) {
+    fileName = fileName.replace(' ', '-');
   fs.writeFile(`./out/${fileName}.json`, JSON.stringify(obj), 'utf8', callback);
 }
