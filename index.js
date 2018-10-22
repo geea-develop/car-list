7bb15bb9027d6e350a2b2448ff603ae90d962d9f
Guy Ambar <guy@meetleo.co>
2018-10-22 14:29:25 +0300

    added body and sub model

--- index.js
+++ index.js
@@ -2,14 +2,12 @@
 var csv = require("fast-csv");
 var fs = require('fs');
 
-var carManufacturers = require('./carManufactureresEng');
-carManufacturers = Object.keys(carManufacturers);
-var carModels = {};
-var carModelsUnique = [];
-var carManufacturersHeb = {};
+// var carManufacturers = require('./carManufactureresEng');
+// carManufacturers = Object.keys(carManufacturers);
+var cars = {};
 
 // const manufacturersEng = require('./carManufactureresEng')
-const carManufacturersEng = require('./out/car-manufacturers-eng');
+// const carManufacturersEng = require('./out/car-manufacturers-eng');
 // Object.keys(manufacturersEng).forEach(carManufacturer => {
 //     manufacturersEng[carManufacturer].forEach(key => {
 //         carManufacturersEng[key] = carManufacturer;
@@ -17,41 +15,114 @@ const carManufacturersEng = require('./out/car-manufacturers-eng');
 // });
 // writeToJSON('car-manufacturers-eng', carManufacturersEng, function(){ console.log('carManufacturersEng.json created');});
 
+const required = [
+    'manufacturerName',
+    'modelSeries',
+    'bodyType',
+    'subModel'
+];
+
+var carManufacturers = {};
+var carModels = {};
+var carBodyTypes = {};
+var carSubModels = {};
+
 csv
- .fromPath("DGAMIM-PRATI.csv", {headers : ["type", "manufacturerNo","manufacturerName","modelNo", "modelName", "modelSeries", "engineCapacity", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ", "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BK", "BL", "BM", "BN", "BO" ]})
+ .fromPath("DGAMIM-PRATI.csv", {headers : ["type", "manufacturerNo","manufacturerName","modelNo", "modelName", "modelSeries", "engineCapacity", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "bodyType", "subModel", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ", "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BK", "BL", "BM", "BN", "BO" ]})
  .on("data", function(data){
    var car = {
      manufacturerNo: data.manufacturerNo,
-     manufacturerName: carManufacturersEng[data.manufacturerNo],
-     modelNo: data.modelNo,
-     modelName: data.modelName.trim(),
+     manufacturerName: data.manufacturerName.trim(),
      modelSeries: data.modelSeries.trim(),
-     engineCapacity: data.engineCapacity
+     bodyType: data.bodyType.trim(),
+     subModel: data.subModel.trim(),
+   };
+
+   car.modelSeriesKey = encodeURIComponent(car.modelSeries);
+   car.bodyTypeKey = encodeURIComponent(car.bodyType);
+   car.subModelKey = encodeURIComponent(car.subModel);
+
+   if (!carManufacturers[car.manufacturerNo]) {
+       cars[car.manufacturerNo] = {};
+       carManufacturers[car.manufacturerNo] = car.manufacturerName;
+       carModels[car.manufacturerNo] = {};
+       carBodyTypes[car.manufacturerNo] = {};
+       carSubModels[car.manufacturerNo] = {};
    }
 
-   carManufacturersHeb[data.manufacturerNo] = data.manufacturerName.trim();
+   if (!cars[car.manufacturerNo][car.modelSeriesKey]) {
+       cars[car.manufacturerNo][car.modelSeriesKey] = {};
+       carModels[car.manufacturerNo][car.modelSeriesKey] = car.modelSeries;
+       carBodyTypes[car.manufacturerNo][car.modelSeriesKey] = {};
+       carSubModels[car.manufacturerNo][car.modelSeriesKey]  = {};
+   }
 
-   if (!carModelsUnique.includes(car.manufacturerName +'_'+ car.modelSeries)) {
-       carModelsUnique.push(car.manufacturerName +'_'+ car.modelSeries)
-       if (!carModels[car.manufacturerName])
-           carModels[car.manufacturerName] = [];
+   if (!cars[car.manufacturerNo][car.modelSeriesKey][car.bodyTypeKey]) {
+       cars[car.manufacturerNo][car.modelSeriesKey][car.bodyTypeKey] = {};
+       carBodyTypes[car.manufacturerNo][car.modelSeriesKey][car.bodyTypeKey] = car.bodyType;
+       carSubModels[car.manufacturerNo][car.modelSeriesKey][car.bodyTypeKey]  = {};
+   }
 
-       carModels[car.manufacturerName].push(car)
+   if (!cars[car.manufacturerNo][car.modelSeriesKey][car.bodyTypeKey][car.subModelKey]) {
+       cars[car.manufacturerNo][car.modelSeriesKey][car.bodyTypeKey][car.subModelKey] = {};
+       carSubModels[car.manufacturerNo][car.modelSeriesKey][car.bodyTypeKey][car.subModelKey] = car.subModel;
    }
 
  })
  .on("end", function(){
     console.log("done reading csv");
 
-     writeToJSON('car-manufacturers', carManufacturersHeb, function(){ console.log(`carManufacturersHeb car-manufacturers created`);})
+    writeToJSON(
+        './out/',
+        'manufacturers-list',
+        carManufacturers,
+        function(){ console.log(`car-manufacturers created`); }
+        );
 
-    Object.keys(carModels).forEach(key => {
-      writeToJSON('models/'+key, carModels[key], function(){ console.log(`carModels for manufacturer ${key} created`);})
+    createDirIfNotExists('./out/manufacturers/');
+
+    Object.keys(carModels).forEach(manufacturer => {
+      writeToJSON(
+          './out/manufacturers/' + manufacturer + '/',
+          'models-list',
+          carModels[manufacturer],
+          function(){ console.log(`car models for manufacturer ${manufacturer} created`);}
+          );
+
+        createDirIfNotExists('./out/manufacturers/' + manufacturer + '/models/');
+
+        Object.keys(carModels[manufacturer]).forEach(iModel => {
+
+            writeToJSON(
+                './out/manufacturers/' + manufacturer + '/models/' + iModel + '/',
+                'body-list',
+                carBodyTypes[manufacturer][iModel],
+                function(){ console.log(`car body for ${manufacturer} ${iModel} created`);}
+                );
+
+            createDirIfNotExists('./out/manufacturers/' + manufacturer + '/models/'+ iModel + '/body/');
+
+            Object.keys(carBodyTypes[manufacturer][iModel]).forEach(iBodyType => {
+                writeToJSON(
+                    './out/manufacturers/' + manufacturer + '/models/' + iModel + '/' + '/body/',
+                    'sub-model-list',
+                    carSubModels[manufacturer][iModel][iBodyType],
+                    function(){ console.log(`car sub models for ${manufacturer} ${iModel} ${iBodyType} created`);}
+                    );
+            });
+        });
     })
 
  });
 
-function writeToJSON(fileName, obj, callback) {
-    fileName = fileName.replace(' ', '-');
-  fs.writeFile(`./out/${fileName}.json`, JSON.stringify(obj), 'utf8', callback);
+function writeToJSON(dir, fileName, obj, callback) {
+    fileName = encodeURIComponent(fileName);
+    createDirIfNotExists(dir);
+    fs.writeFile(`${dir}${fileName}.json`, JSON.stringify(obj), 'utf8', callback);
+}
+
+function createDirIfNotExists(dir) {
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
 }
